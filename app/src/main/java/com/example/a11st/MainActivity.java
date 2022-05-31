93bb9ad039fdcb18c939274dcfd962f610bb18ad
주현서 <joohyunseo@juhyeonseoui-MacBookPro.local>
2022-05-31 10:07:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a11st/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.a11st;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+
+    static RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit_Text);
+        textView = findViewById(R.id.Text_View);
+
+        Button button = findViewById(R.id.But_ton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeRequest();
+            }
+        });
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+    }
+
+    public void makeRequest(){
+        String url = editText.getText().toString();
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response){
+                        println("응답-> "+ response);
+                        processResponse(response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    println("에러-> "+error.getMessage());
+
+                }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+
+                return params;
+            }
+        };
+
+        request.setShouldCache(false);
+        requestQueue.add(request);
+        println("요청보냄.");
+    }
+
+    public void processResponse(String response){
+        Gson gson = new Gson();
+        MovieList movieList = gson.fromJson(response,MovieList.class);
+        println("영화 정보의 수 : "+ movieList.boxOfficeResult.dailyBoxOfficeList.size());
+
+    }
+
+    public void println(String data){
+        textView.append(data + "\n");
+    }
+}
